0cfaa0f156471c1614beabdd92f95f5be32638f9
Jean-Baptiste Sossou <sojeba008@gmail.com>
2023-06-04 16:19:13 +0200

    Code refactoring and MetaBase init 	- add requirements.txt 	- add .env

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # LM10StatsViz
 Lionel Messi Club goal Stats Visualization
+
+
+pip install -r requirements.txt
--- main.py
+++ main.py
@@ -4,92 +4,22 @@ Created on Fri Jun  2 00:00:18 2023
 
 @author: Jean-Baptiste
 """
-def persitDataInDB(table_name, data):
-    postgres_url = "jdbc:postgresql://192.168.8.146:5432/messiStatsDW"
-    properties = {
-        "user": "postgres",
-        "password": "postgres",
-        "driver": "org.postgresql.Driver"
-    }
-    data_writer = DataFrameWriter(data)
-    data_writer.jdbc(url=postgres_url, table=table_name, mode="overwrite", properties=properties)
 
-
-
+import os
+from dotenv import load_dotenv
 import findspark
-findspark.init()
-findspark.add_packages(["org.postgresql:postgresql:42.6.0"])
 from pyspark.sql import SparkSession
-
-from pyspark.sql.functions import split, regexp_replace
-
-
-from pyspark.sql.functions import monotonically_increasing_id, lit
-
+from pyspark.sql.functions import split, regexp_replace, monotonically_increasing_id, lit
 from pyspark.sql.functions import udf
 from pyspark.sql.types import StringType
 from pyspark.sql import DataFrameWriter
 
+findspark.init()
+findspark.add_packages(["org.postgresql:postgresql:42.6.0"])
 
-spark = SparkSession.builder.getOrCreate()
-data = spark.read.csv("c:/Users/sojeb/Downloads/data.csv", header=True, encoding="UTF-8")
-data = data.withColumn("Goal_assist", regexp_replace(data["Goal_assist"], "(\\t|null)$", ""))
-data = data.withColumn("Date", regexp_replace(data["Date"],"-","/" ))
-data.printSchema()
+dotenv_path = os.path.join(os.getcwd(), '.env')
+load_dotenv(dotenv_path)
 
-allCompetitions = data.select("Competition").withColumnRenamed("Competition", "name").distinct().withColumn("pk_competition", monotonically_increasing_id() + 1)
-allCompetitions.show()
-
-##
-data = data.join(allCompetitions, allCompetitions.name == data.Competition, "inner").drop("name")
-##
-
-### Dim date
-data = data.withColumn('month', split(data["date"], "/").getItem(0))
-data = data.withColumn('day', split(data["date"], "/").getItem(1))
-data = data.withColumn('year', split(data["date"], "/").getItem(2))
-data = data.withColumn("pk_goal", monotonically_increasing_id() + 1)
-allDates = data.select("day", "month", "year").distinct().withColumn("pk_date", monotonically_increasing_id() + 1)
-allDates.show()
-
-postgres_url = "jdbc:postgresql://192.168.8.146:5432/messiStatsDW"
-properties = {
-    "user": "postgres",
-    "password": "postgres",
-    "driver": "org.postgresql.Driver"
-}
-table_name = "public.dim_date"
-persitDataInDB(table_name, allDates)
-
-##
-data = data.join(allDates, [allDates.month == data.month, allDates.day == data.day, allDates.year == data.year], "inner").drop("name")
-##
-####################
-
-
-####Dim goal type
-allTypes = data.select("Type").withColumnRenamed("Type", "name").distinct().withColumn("pk_type", monotonically_increasing_id() + 1)
-allTypes.show()
-table_name = "public.dim_type"
-persitDataInDB(table_name, allTypes)
-
-##
-data = data.join(allTypes, [allTypes.name == data.Type], "inner").drop("name")
-##
-#####################
-
-###Dim assist
-allGoalAssist = data.select("Goal_assist").withColumnRenamed("Goal_assist", "name").distinct().withColumn("pk_goal_assist", monotonically_increasing_id() + 1)
-#allGoalAssist = allGoalAssist.withColumn("name", regexp_replace(allGoalAssist["name"], "(\\t|null)$", ""))
-allGoalAssist.show()
-table_name = "public.dim_goal_assist"
-persitDataInDB(table_name, allGoalAssist)
-##
-data = data.join(allGoalAssist, [allGoalAssist.name == data.Goal_assist], "inner").drop("name")
-##
-################
-
-###Dim result
 def result(score):
     if  int(score.split(":")[0])>int(score.split(":")[1]):
         return "Win"
@@ -97,73 +27,139 @@ def result(score):
         return "Draw" 
     else :
         return "Defeat"
-result_udf = udf(result, StringType())
-data = data.withColumn("result", result_udf(data["At_score"]))
-allResult = data.select("result").distinct().withColumn("pk_result", monotonically_increasing_id() + 1)
-table_name = "public.dim_result"
-persitDataInDB(table_name, allResult)
-
-##
-data = data.join(allResult, [allResult.result == data.result], "inner").drop("result")
-##
-##############
-
-####dim club
-allMessiClubs = data.select("Club").distinct().withColumnRenamed("Club", "name")
-allMessiOponents = data.select("Opponent").distinct().withColumnRenamed("Opponent", "name")
-allClub = allMessiClubs.unionByName(allMessiOponents)
-allClub = allClub.withColumn("pk_club", monotonically_increasing_id() + 1)
-allClub.show()
-table_name = "public.dim_club"
-persitDataInDB(table_name, allResult)
-
-##
-data = data.join(allClub, [allClub.name == data.Opponent], "inner").drop("name")
-##
-##############
-####Dim venue
-allVenue = data.select("Venue").distinct()
-allVenue = allVenue.withColumn("pk_venue", monotonically_increasing_id() + 1)
-allVenue.show()
-table_name = "public.dim_Venue"
-persitDataInDB(table_name, allVenue)
-
-##
-data = data.join(allVenue, [allVenue.Venue == data.Venue], "inner").drop("Venue")
-##
-####################
 
 
-####dim playing postion
-allMessiPlayingPosition = data.select("Playing_Position").distinct().withColumn("pk_playing_position", monotonically_increasing_id() + 1)
-allMessiPlayingPosition = allMessiPlayingPosition.withColumn("Playing_Position", regexp_replace(data["Playing_Position"], " ", "")).withColumnRenamed("Playing_Position", "code")
-allMessiPlayingPosition.show()
-table_name = "public.dim_playing_position"
-persitDataInDB(table_name, allMessiPlayingPosition)
-##
-data = data.join(allMessiPlayingPosition, [allMessiPlayingPosition.code == data.Playing_Position], "inner")
-##
-##################
-
-
-###Dim minute goal
-AllMinuteGoal = data.select("Minute").distinct().withColumnRenamed("Minute", "minute_str")
 def convertMinuteToInt(minute):
     if "+" not in minute:
         return minute
     else:
         return int(minute.split("+")[0])+int(minute.split("+")[1])
-convertMinuteToInt_udf = udf(convertMinuteToInt, StringType())
-AllMinuteGoal = AllMinuteGoal.withColumn("minute_int", convertMinuteToInt_udf(AllMinuteGoal["minute_str"])).withColumn("pk_minute", monotonically_increasing_id() + 1)
-AllMinuteGoal.show()
-table_name = "dim_minute_goal"
-persitDataInDB(table_name, allMessiPlayingPosition)
-##
-data = data.join(AllMinuteGoal, [AllMinuteGoal.minute_str == data.Minute], "inner")
-##
+    
+
+class LM10ClubGoalStatsETL:
+    def __init__(self):
+      
+        self.postgres_url = os.getenv("postgres_url")
+        self.properties = {
+            "user": os.getenv("user"),
+            "password": os.getenv("password"),
+            "driver": os.getenv("driver")
+        }
+        
+        spark = SparkSession.builder.getOrCreate()
+        self.data = spark.read.csv("input/data.csv", header=True, encoding="UTF-8")
+        self.data.printSchema()
+        self.data = self.data.withColumn("Goal_assist", regexp_replace(self.data["Goal_assist"], "(\\t|null)$", ""))
+        self.data = self.data.withColumn("Date", regexp_replace(self.data["Date"],"-","/" ))
+        
+    
+    def persitDataInDB(self, table_name, data):
+        data_writer = DataFrameWriter(data)
+        data_writer.jdbc(url=self.postgres_url, table=table_name, mode="overwrite", properties=self.properties)
+
+    def loadDimCompetiton(self):
+        self.allCompetitions = self.data.select("Competition").withColumnRenamed("Competition", "name").distinct().withColumn("pk_competition", monotonically_increasing_id() + 1)
+        self.allCompetitions.show()
+        table_name = "public.dim_competition"
+        self.persitDataInDB(table_name, self.allCompetitions)
+    
+    def loadDimDate(self):
+        self.data = self.data.withColumn('month', split(self.data["date"], "/").getItem(0))
+        self.data = self.data.withColumn('day', split(self.data["date"], "/").getItem(1))
+        self.data = self.data.withColumn('year', split(self.data["date"], "/").getItem(2))
+        self.data = self.data.withColumn("pk_goal", monotonically_increasing_id() + 1)
+        self.allDates = self.data.select("day", "month", "year").distinct().withColumn("pk_date", monotonically_increasing_id() + 1)
+        self.allDates.show()
+        table_name = "public.dim_date"
+        self.persitDataInDB(table_name, self.allDates)
+        
+    def loadDimType(self): 
+        self.allTypes = self.data.select("Type").withColumnRenamed("Type", "name").distinct().withColumn("pk_type", monotonically_increasing_id() + 1)
+        self.allTypes.show()
+        table_name = "public.dim_type"
+        self.persitDataInDB(table_name, self.allTypes)
+        #self.data = self.data.join(self.allTypes, [self.allTypes.name == self.data.Type], "inner").drop("name")
 
 
+    def loadDimGoalAssist(self):
+        self.allGoalAssist = self.data.select("Goal_assist").withColumnRenamed("Goal_assist", "name").distinct().withColumn("pk_goal_assist", monotonically_increasing_id() + 1)
+        self.allGoalAssist = self.allGoalAssist.withColumn("name", regexp_replace(self.allGoalAssist["name"], "(\\t|null)$", ""))
+        self.allGoalAssist.show()
+        table_name = "public.dim_goal_assist"
+        self.persitDataInDB(table_name, self.allGoalAssist)        
 
-table_name = "fact_goal_messi"
-messiGoalFact = data.select("pk_date", "pk_competition", "pk_type", "pk_goal_assist", "pk_result", "pk_club", "pk_venue", "pk_playing_position", "pk_minute").withColumn("goal",  lit(1))
-persitDataInDB(table_name, messiGoalFact)
+    def loadDimResult(self):
+        result_udf = udf(result, StringType())
+        self.data = self.data.withColumn("result", result_udf(self.data["At_score"]))
+        self.allResult = self.data.select("result").distinct().withColumn("pk_result", monotonically_increasing_id() + 1)
+        table_name = "public.dim_result"
+        self.persitDataInDB(table_name, self.allResult)
+
+    def loadDimClub(self):
+        self.allMessiClubs = self.data.select("Club").distinct().withColumnRenamed("Club", "name")
+        self.allMessiOponents = self.data.select("Opponent").distinct().withColumnRenamed("Opponent", "name")
+        self.allClub = self.allMessiClubs.unionByName(self.allMessiOponents)
+        self.allClub = self.allClub.withColumn("pk_club", monotonically_increasing_id() + 1)
+        self.allClub.show()
+        table_name = "public.dim_club"
+        self.persitDataInDB(table_name, self.allClub)
+
+    def loadDimVenue(self):
+        self.allVenue = self.data.select("Venue").distinct()
+        self.allVenue = self.allVenue.withColumn("pk_venue", monotonically_increasing_id() + 1)
+        self.allVenue.show()
+        table_name = "public.dim_Venue"
+        self.persitDataInDB(table_name, self.allVenue)
+
+    def loadDimPlayingPosition(self):
+        self.allMessiPlayingPosition = self.data.select("Playing_Position").distinct().withColumn("pk_playing_position", monotonically_increasing_id() + 1)
+        self.allMessiPlayingPosition = self.allMessiPlayingPosition.withColumn("Playing_Position", regexp_replace(self.data["Playing_Position"], " ", "")).withColumnRenamed("Playing_Position", "code")
+        self.allMessiPlayingPosition.show()
+        table_name = "public.dim_playing_position"
+        self.persitDataInDB(table_name, self.allMessiPlayingPosition)
+        
+    def loadMinuteGoal(self): 
+        self.allMinuteGoal = self.data.select("Minute").distinct().withColumnRenamed("Minute", "minute_str")
+        convertMinuteToInt_udf = udf(convertMinuteToInt, StringType())
+        self.allMinuteGoal = self.allMinuteGoal.withColumn("minute_int", convertMinuteToInt_udf(self.allMinuteGoal["minute_str"])).withColumn("pk_minute", monotonically_increasing_id() + 1)
+        self.allMinuteGoal.show()
+        table_name = "public.dim_minute_goal"
+        self.persitDataInDB(table_name, self.allMinuteGoal)
+
+    def loadFactMessiGoal(self):     
+        self.data = self.data.join(self.allCompetitions, self.allCompetitions.name == self.data.Competition, "inner").drop("name")
+        self.allCompetitions.unpersist()
+        self.data = self.data.join(self.allDates, [self.allDates.month == self.data.month, self.allDates.day == self.data.day, self.allDates.year == self.data.year], "inner").drop("name")
+        self.allDates.unpersist()
+        self.data = self.data.join(self.allTypes, [self.allTypes.name == self.data.Type], "inner").drop("name")
+        self.allTypes.unpersist()
+        self.data = self.data.join(self.allGoalAssist, [self.allGoalAssist.name == self.data.Goal_assist], "inner").drop("name")
+        self.allGoalAssist.unpersist()
+        self.data = self.data.join(self.allResult, [self.allResult.result == self.data.result], "inner").drop("result")
+        self.allResult.unpersist()
+        self.data = self.data.join(self.allClub, [self.allClub.name == self.data.Opponent], "inner").drop("name")
+        self.allClub.unpersist()
+        self.data = self.data.join(self.allVenue, [self.allVenue.Venue == self.data.Venue], "inner").drop("Venue")
+        self.allVenue.unpersist()
+        self.data = self.data.join(self.allMessiPlayingPosition, [self.allMessiPlayingPosition.code == self.data.Playing_Position], "inner")
+        self.allMessiPlayingPosition.unpersist()
+        self.data = self.data.join(self.allMinuteGoal, [self.allMinuteGoal.minute_str == self.data.Minute], "inner")
+        
+        table_name = "public.fact_goal"
+        factMessiGoal = self.data.select("pk_date", "pk_competition", "pk_type", "pk_goal_assist", "pk_result", "pk_club", "pk_venue", "pk_playing_position", "pk_minute").withColumn("goal",  lit(1))
+        self.persitDataInDB(table_name, factMessiGoal)
+        factMessiGoal.unpersist()
+        self.data.unpersist()
+        
+        
+etl = LM10ClubGoalStatsETL()
+etl.loadDimCompetiton()
+etl.loadDimDate()
+etl.loadDimType()
+etl.loadDimGoalAssist()
+etl.loadDimResult()
+etl.loadDimClub()
+etl.loadDimVenue()
+etl.loadDimPlayingPosition()
+etl.loadMinuteGoal()
+etl.loadFactMessiGoal()
